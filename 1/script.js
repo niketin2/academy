0a409b777f38ba09e8fbc7b3dceb9685fec596d9
owarimonogatarini@gmail.com <owarimonogatarini@gmail.com>
2018-06-27 20:25:38 +0300

    This first lesson

---
+++ 1/script.js
@@ -0,0 +1,18 @@
+var n = prompt("Ваш бюджет");
+var nameShopString = prompt("Назва магазину");
+var nameGoods = prompt("Шо будете купляти?");
+var nameGoods1 = prompt("Шо будете купляти?");
+var nameGoods2 = prompt("Шо будете купляти?");
+
+var obj = {
+	budjet: n,
+	nameShop: nameShopString,
+	goods: goodsObj = [nameGoods, nameGoods1, nameGoods2],
+	shopGoods: namePeople = {
+		people1: "Piter",
+		people2: "MaxLoh"
+	} 
+};
+
+
+console.log(obj);
